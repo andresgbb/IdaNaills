f43e7e142a425e4b3d94955d4845d59f1eb50c6a
andresgb <a.garcia1smixnuria2020@gmail.com>
2024-02-29 20:55:28 +0100

    modifie table reservas

--- database/migrations/2024_02_09_220423_create_reservas_table.php
+++ database/migrations/2024_02_09_220423_create_reservas_table.php
@@ -15,11 +15,10 @@ return new class extends Migration
         Schema::create('reservas', function (Blueprint $table) {
             $table->id();
             $table->date('fecha');
+            $table->time('hora');
             $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('servicio_id');
 
             $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('servicio_id')->references('id')->on('servicios');
             $table->timestamps();
         });
     }
