cdafc24136341a1995a02ab6f16a4e3be97a9684
andresgb <a.garcia1smixnuria2020@gmail.com>
2024-03-07 18:35:23 +0100

    Modified Admin Controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Models\Servicio;
+
 
 class AdminController extends Controller
 {
@@ -13,4 +15,25 @@ class AdminController extends Controller
             'user' => $request->user(),
         ]);
     }
+    public function crearServicio(Request $request){
+
+        $datosFormulario = $request->all();
+        $tiempo=$datosFormulario['duracion'];
+
+        // Formatear los minutos en un formato de tiempo compatible con la base de datos
+        $duracion = sprintf('%02d:%02d:00', floor($tiempo / 60), $tiempo % 60);
+
+        $servicio= new Servicio();
+        $servicio->nombre = $datosFormulario['nombre'];
+        $servicio->precio = $datosFormulario['precio'];
+        $servicio->duracion= $duracion;
+        $servicio->save();
+        return view('home');
+     }
+     public function eliminar($id){
+        $servicio = Servicio::findOrFail($id);
+        $servicio->delete();
+        return redirect()->back()->with('success', 'Servicio Eliminado correctamente');
+     }
+
 }
